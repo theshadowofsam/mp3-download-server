105f6b883eeab00524151fe74cfdc06613997bb1
sam <theshadowofsam@gmail.com>
2021-11-29 17:40:32 -0500

    added GET http request

--- server.py
+++ server.py
@@ -3,3 +3,16 @@ server.py
 Samuel Lee
 11/29/2021
 """
+import requests
+import yt_search
+import asyncio
+
+format = 250
+
+def main(url):
+    url = yt_search.search(url)
+    r = requests.get(url, allow_redirects=True)
+    print(r)
+
+if __name__ == "__main__":
+    main("https://www.youtube.com/watch?v=kGxiZNbjwGI")
