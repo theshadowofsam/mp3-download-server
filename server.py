63d0c36538c963c224379e89776b4abf3b9428cb
sam <theshadowofsam@gmail.com>
2021-11-29 17:51:50 -0500

    added file writing for response

--- server.py
+++ server.py
@@ -11,8 +11,12 @@ format = 250
 
 def main(url):
     url = yt_search.search(url)
-    r = requests.get(url[1], allow_redirects=True)
+    r = requests.get(url[1], allow_redirects=True, stream=True)
     print(r)
+
+    with open("audio.weba", 'wb') as fd:
+        for i in r.iter_content():
+            fd.write(i)
 
 if __name__ == "__main__":
     main("https://www.youtube.com/watch?v=kGxiZNbjwGI")
